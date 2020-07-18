4f2f8c1418ae739b45926f0b12e83610e16b88b6
Beijia(Frances) Yu <43866367+yufrances90@users.noreply.github.com>
2020-07-18 16:55:17 -0700

    chore: Programatically set password and username on the datasource object

---
+++ critter/src/main/java/com/udacity/jdnd/course3/critter/DataSourceConfig.java
@@ -0,0 +1,30 @@
+package com.udacity.jdnd.course3.critter;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @ConfigurationProperties("datasource.mysql")
+    public DataSource getDataSource(DataSourceProperties properties) {
+
+        DataSourceBuilder dsb = DataSourceBuilder.create();
+
+        dsb.username("sa");
+        dsb.password(this.securePasswordService());
+
+        return dsb.build();
+    }
+
+    private String securePasswordService() {
+        return "sa1234";
+    }
+}
